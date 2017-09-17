b835a08ca1523b08aa82f7b35a840d8694bafd3e
Deepthi <deepthi.vm@gmail.com>
2017-09-17 02:33:23 -0400

    first commit

---
+++ src/Assignment7_1/Assignment7_1.java
@@ -0,0 +1,16 @@
+package Assignment7_1;
+
+public class Assignment7_1 {
+	public static void main(String args[]){
+		int number=123;
+		//String price = String.valueOf(123);
+		Integer intInstance = new Integer(number);  
+		String numberAsString = intInstance.toString();
+		
+		System.out.println("Converting integer to String :"+numberAsString);
+	}
+	
+	
+
+	
+}
